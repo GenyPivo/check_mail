130a03c0523d08a7924d5767b880912126a8b004
Evgeniy Pivovarov <evgeniy.pivovarov@uptech.team>
2017-09-11 13:40:25 +0300

    Api doc

--- app/controllers/api/v1/check_mail_controller.rb
+++ app/controllers/api/v1/check_mail_controller.rb
@@ -4,6 +4,35 @@ class Api::V1::CheckMailController < ApplicationController
   before_action :validate_params, only: :index
   before_action :init_check_mail_services, only: :index
 
+=begin
+  @api {get} /check_mail Request available emails
+  @apiName GetUser
+  @apiGroup User
+  @apiParam {String} first_name User first name.
+  @apiParam {String} last_name User last name.
+  @apiParam {String} company_title Company name.
+  @apiParam {Integer} [page] Current page.
+  @apiParam {Integer} [per_page] Data per page.
+
+
+  @apiSuccess {String} status Request status.
+  @apiSuccess {Array} response Mail list.
+
+  @apiSuccessExample {json} Success-Response:
+                   {
+                     "status": "success"
+                     "response": [
+                        'mail1@mail.com', 'mail2@mail.com', 'mail3@mail.com'
+                      ]
+                   }
+  @apiErrorExample {json} Error-Response:
+       HTTP/1.1 422 Unprocessable entity
+       {
+         "status": "error",
+         "response":{"company_title":["can't be blank"]}}
+       }
+=end
+
   def index
     @available_mailboxes = @available_mailboxes.paginate(per_page: params[:per_page] || DEFAULT_PER_PAGE,
                                                          page: params[:page])
@@ -13,17 +42,17 @@ class Api::V1::CheckMailController < ApplicationController
   private
 
   def validate_params
-    @request = MailRequest.new(required_params)
-    unless @request.valid?
-      render json: { status: :error, response: request.errors }
+    @req = MailRequest.new(required_params)
+    unless @req.valid?
+      render json: { status: :error, response: @req.errors }, status: 422
     end
   end
 
   def init_check_mail_services
-    mail_generator = MailGeneratorService.new(@request.first_name,
-                                              @request.last_name)
+    mail_generator = MailGeneratorService.new(@req.first_name,
+                                              @req.last_name)
     mx_checker = MxCheckerService.new
-    mx_records = mx_checker.collect_mx_by_company_title(@request.company_title)
+    mx_records = mx_checker.collect_mx_by_company_title(@req.company_title)
     generated_emails = mail_generator.generate_mail_addresses(mx_records.keys)
     @available_mailboxes = CheckMailByMxService.new(generated_emails, mx_records).available
   end
---
+++ public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "ralabs",
+  "version": "0.1.0",
+  "description": "mail checker via mx app",
+  "title": "mail checker",
+  "url": "http://localhost:3000/api/v1",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-09-11T10:40:22.660Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
